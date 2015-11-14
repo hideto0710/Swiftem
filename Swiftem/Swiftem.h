b1896c2c62f89dd8032438f28d90ed3fc113aac0
hideto0710 <h.inamura0710@gmail.com>
2015-11-14 10:15:56 +0900

    Create xcode project.

---
+++ Swiftem/Swiftem.h
@@ -0,0 +1,19 @@
+//
+//  Swiftem.h
+//  Swiftem
+//
+//  Created by InamuraHideto on 11/14/15.
+//  Copyright © 2015 InamuraHideto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swiftem.
+FOUNDATION_EXPORT double SwiftemVersionNumber;
+
+//! Project version string for Swiftem.
+FOUNDATION_EXPORT const unsigned char SwiftemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swiftem/PublicHeader.h>
+
+
